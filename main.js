dfd0dd1a3c2588a97c7a0239b06c4d5c5f873884
Levrk <levrolandkalb@gmail.com>
2023-03-08 00:04:28 -0500

    added select() functions

--- main.js
+++ main.js
@@ -1,3 +1,172 @@
+
+
+function selectBio() {
+    //set vars
+    var bio = document.getElementById("bio");
+    var thisButton = document.getElementById("bio-button");
+    var thatButton = document.getElementById("links-button");
+    var links = document.getElementById("links");
+    
+
+    //make changes to indicate selection
+    thisButton.classList.add("button-selected")
+    thatButton.classList.remove("button-selected")
+    links.classList.remove("content-selected");
+    bio.classList.add("content-selected");
+
+    //updating global vars
+    bioSelected=true;
+    linksSelected=false;
+}
+
+function selectLinks() {
+    //set vars
+    var links = document.getElementById("links");
+    var thisButton = document.getElementById("links-button");
+    var thatButton = document.getElementById("bio-button");
+    var bio = document.getElementById("bio");
+  
+
+
+    //make changes to indicate selection
+    thisButton.classList.add("button-selected")
+    thatButton.classList.remove("button-selected")
+    bio.classList.remove("content-selected");
+    links.classList.add("content-selected");
+
+    //updating global vars
+    linksSelected=true;
+    bioSelected=false;
+}
+
+function selectResume() {
+    //set vars
+    var resume = document.getElementById("resume");
+    var thisButton = document.getElementById("resume-button");
+    var thatButton = document.getElementById("skills-button");
+    var skills = document.getElementById("skills");
+    
+
+
+    //make changes to indicate selection
+    thisButton.classList.add("button-selected")
+    thatButton.classList.remove("button-selected") 
+    
+    skills.classList.remove("content-selected");
+    resume.classList.add("content-selected");
+
+    //updating global vars
+    resumeSelected=true;
+    skillsSelected=false;
+}
+
+function selectSkills() {
+    //set vars
+    var skills = document.getElementById("skills");
+    var thisButton = document.getElementById("skills-button");
+    var thatButton = document.getElementById("resume-button");
+    var resume = document.getElementById("resume");
+    
+
+
+
+    //make changes to indicate selection
+    thisButton.classList.add("button-selected")
+    thatButton.classList.remove("button-selected")
+    resume.classList.remove("content-selected");
+    skills.classList.add("content-selected");
+
+    //updating global vars
+    skillsSelected=true;
+    resumeSelected=false;
+}
+
+
+
+
+function selectProject1() {
+    //set vars
+    var project1 = document.getElementById("project1");
+    var project1Button = document.getElementById("project1-button");
+    var project2Button = document.getElementById("project2-button");
+    var project3Button = document.getElementById("project3-button");
+    var project2 = document.getElementById("project2");
+    var project3 = document.getElementById("project3");
+    
+    //if resume is selected already
+    
+
+
+    //make changes to indicate selection
+    project2Button.classList.remove("button-selected");
+    project3Button.classList.remove("button-selected");
+    project2.classList.remove("content-selected");
+    project3.classList.remove("content-selected");
+
+    project1.classList.add("content-selected");
+    project1Button.classList.add("button-selected");
+
+}
+
+function selectProject2() {
+    //set vars
+    var project1 = document.getElementById("project1");
+    var project1Button = document.getElementById("project1-button");
+    var project2Button = document.getElementById("project2-button");
+    var project3Button = document.getElementById("project3-button");
+    var project2 = document.getElementById("project2");
+    var project3 = document.getElementById("project3");
+    
+    //if resume is selected already
+    
+
+
+    //make changes to indicate selection
+    project1Button.classList.remove("button-selected");
+    project3Button.classList.remove("button-selected");
+    project1.classList.remove("content-selected");
+    project3.classList.remove("content-selected");
+
+    project2.classList.add("content-selected");
+    project2Button.classList.add("button-selected");
+
+}
+
+function selectProject3() {
+    //set vars
+    var project1 = document.getElementById("project1");
+    var project1Button = document.getElementById("project1-button");
+    var project2Button = document.getElementById("project2-button");
+    var project3Button = document.getElementById("project3-button");
+    var project2 = document.getElementById("project2");
+    var project3 = document.getElementById("project3");
+    
+    //if resume is selected already
+    
+
+
+    //make changes to indicate selection
+    project1Button.classList.remove("button-selected");
+    project2Button.classList.remove("button-selected");
+    project1.classList.remove("content-selected");
+    project2.classList.remove("content-selected");
+
+    project3.classList.add("content-selected");
+    project3Button.classList.add("button-selected");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 document.addEventListener('mousemove', function(event) {
     
     var X = ((event.clientX / window.innerWidth) * 200)-50;
@@ -12,3 +181,14 @@ document.addEventListener('mousemove', function(event) {
     
 }
 });
+
+window.onload = function() {
+    // Execute your functions here
+    onLoad();
+  };
+
+function onLoad() {
+    selectBio();
+    selectProject1();
+    selectResume();
+}
